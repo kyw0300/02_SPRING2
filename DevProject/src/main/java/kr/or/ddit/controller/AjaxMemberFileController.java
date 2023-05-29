639fb9e36817e5879008c07bf93527c7880e4f53
kyw0300 <kyw0300@naver.com>
2023-05-30 02:23:01 +0900

    230530 commit

--- DevProject/src/main/java/kr/or/ddit/controller/AjaxMemberFileController.java
+++ DevProject/src/main/java/kr/or/ddit/controller/AjaxMemberFileController.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.multipart.MultipartFile;
 
 import lombok.extern.slf4j.Slf4j;
