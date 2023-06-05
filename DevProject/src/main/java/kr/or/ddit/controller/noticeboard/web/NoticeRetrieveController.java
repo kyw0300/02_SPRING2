1375a4baae35d87d27a7797f2358a76811ca8941
kyw95 <kyw0300@naver.com>
2023-06-05 17:31:46 +0900

    230605 파일 업로드 CommonsMultipartResolver 사용방식

--- DevProject/src/main/java/kr/or/ddit/controller/noticeboard/web/NoticeRetrieveController.java
+++ DevProject/src/main/java/kr/or/ddit/controller/noticeboard/web/NoticeRetrieveController.java
@@ -1,14 +1,19 @@
 package kr.or.ddit.controller.noticeboard.web;
 
+import java.util.List;
+
 import javax.inject.Inject;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import kr.or.ddit.controller.noticeboard.service.INoticeService;
 import kr.or.ddit.vo.NoticeVO;
+import kr.or.ddit.vo.PaginationInfoVO;
 import lombok.extern.slf4j.Slf4j;
 
 @Controller
@@ -19,9 +24,34 @@ public class NoticeRetrieveController {
 	@Inject
 	private INoticeService noticeService;
 
-	@GetMapping("/list.do")
-	public String noticeList() {
+	@RequestMapping("/list.do")
+	public String noticeList(
+			@RequestParam(name="page", required = false, defaultValue = "1") int currentPage,
+			@RequestParam(required = false, defaultValue = "title") String searchType,
+			@RequestParam(required = false) String searchWord,
+			Model model
+			) {
 		log.info("noticeList() 실행...!");
+		
+		PaginationInfoVO<NoticeVO> pagingVO = new PaginationInfoVO<NoticeVO>();
+		
+		// 검색기능 추가 시 활용
+		if(StringUtils.isNoneBlank(searchWord)) {
+			pagingVO.setSearchType(searchType);
+			pagingVO.setSearchWord(searchWord);
+			model.addAttribute("searchType", searchType);
+			model.addAttribute("searchWord", searchWord);
+		}
+		
+		pagingVO.setCurrentPage(currentPage); // startRow, endRow, startPage, endPage가 결정!
+		int totalRecord = noticeService.selectNoticeCount(pagingVO);
+		pagingVO.setTotalRecord(totalRecord);
+		
+		List<NoticeVO> dataList = noticeService.selectNoticeList(pagingVO);
+		pagingVO.setDataList(dataList);
+		
+		model.addAttribute("pagingVO", pagingVO);
+		
 		return "notice/list";
 	}
 	
---
+++ DevProject/src/main/webapp/WEB-INF/views/item2/register.jsp
@@ -0,0 +1,51 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.0/jquery.min.js"></script>
+<body>
+	<h2>File Register</h2>
+	<form action="/item2/register" method="post" enctype="multipart/form-data">
+		<table>
+			<tr>
+				<td>상품명</td>
+				<td>
+					<input type="text" name="itemName" id="itemName"/>
+				</td>
+			</tr>
+			<tr>
+				<td>가격</td>
+				<td>
+					<input type="text" name="price" id="price"/>
+				</td>
+			</tr>
+			<tr>
+				<td>파일</td>
+				<td>
+					<input type="file" name="pictures" id="pictures1"/>
+				</td>
+			</tr>
+			<tr>
+				<td>파일</td>
+				<td>
+					<input type="file" name="pictures" id="pictures2"/>
+				</td>
+			</tr>
+			<tr>
+				<td>개요</td>
+				<td>
+					<textarea rows="10" cols="20" name="description"></textarea>
+				</td>
+			</tr>
+		</table>
+		<div>
+			<button type="submit" id="btnRegister">Register</button>
+			<button type="button" id="btnList" onclick="javascript:location.href='/item2/list'">List</button>
+		</div>
+	</form>
+</body>
+</html>
