af16c0f8aa5e838e04fcfd935f6329bf0c21e220
kyw95 <kyw0300@naver.com>
2023-06-07 16:56:21 +0900

    230607 file업로드 처리

--- DevProject/src/main/java/kr/or/ddit/controller/file/item02/service/ItemServiceImpl2.java
+++ DevProject/src/main/java/kr/or/ddit/controller/file/item02/service/ItemServiceImpl2.java
@@ -26,4 +26,29 @@ public class ItemServiceImpl2 implements ItemService2 {
 		
 	}
 
+	@Override
+	public Item2 read(int itemId) {
+		return mapper.read(itemId);
+	}
+
+	@Override
+	public String getPicture(int itemId) {
+		return mapper.getPicture(itemId);
+	}
+
+	@Override
+	public String getPicture2(int itemId) {
+		return mapper.getPicture2(itemId);
+	}
+
+	@Override
+	public void modify(Item2 item) {
+		mapper.modify(item);
+	}
+
+	@Override
+	public void remove(int itemId) {
+		mapper.remove(itemId);
+	}
+
 }
