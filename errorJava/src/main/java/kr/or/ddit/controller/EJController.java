619d9dd035b2f4bef7f9fa9b59b8bb35beef7f47
kyw95 <kyw0300@naver.com>
2023-06-10 17:00:09 +0900

    230610 tiles를 이용한 템플릿 공통 페이지 분리

---
+++ errorJava/src/main/java/kr/or/ddit/controller/EJController.java
@@ -0,0 +1,20 @@
+package kr.or.ddit.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/main")
+public class EJController {
+
+	@GetMapping("/login")
+	public String loginForm() {
+		return "page-login";
+	}
+	
+	@GetMapping("/index")
+	public String index() {
+		return "index";
+	}
+}
